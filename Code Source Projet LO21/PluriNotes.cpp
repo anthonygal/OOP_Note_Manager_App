5565032c5155c92d4b5bd55232ef50cf695c1b6b
anthonygal <anthony.galtier@gmail.com>
2017-05-17 16:00:13 +0200

    ajout fichier timing et premieres declarations de classes dans le fichier plurinotes.h

--- Code Source Projet LO21/PluriNotes.cpp
+++ Code Source Projet LO21/PluriNotes.cpp
@@ -1,11 +1,1 @@
-//
-//  PluriNotes.cpp
-//  Code Source Projet LO21
-//
-//  Created by Anthony Galtier on 17/05/2017.
-//  Copyright © 2017 Antoine Schleigel, Pierre Lucas, Anthony Galtier. All rights reserved.
-//
-
-#include <stdio.h>
-
-
+#include "PluriNotes.h"
--- Code Source Projet LO21/main.cpp
+++ Code Source Projet LO21/main.cpp
@@ -1,12 +1,4 @@
-﻿//
-//  main.cpp
-//  Code Source Projet LO21
-//
-//  Created by Anthony Galtier on 17/05/2017.
-//  Copyright © 2017 Antoine Schleigel, Pierre Lucas, Anthony Galtier. All rights reserved.
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here... modification test
