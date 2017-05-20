e72079f4585e46c0f3be2c3f6f002c986d20fb48
pierreLucas40 <pn.lucas@free.fr>
2017-05-20 22:04:12 +0200

    petite modif dans parametre de Multimedia

--- Code Source Projet LO21/PluriNotes.h
+++ Code Source Projet LO21/PluriNotes.h
@@ -25,25 +25,25 @@ public:
     virtual ~Note();
 };
 
-/* 
+/*
  COMMENTAIRES A PRENDRE EN COMPTE:
     -Pour la gestion des type dates horaire etc... on reprendra les fichiers timing.h et timing.cpp donnés en ressources lors du TD7
     -Les accesseurs en edition créeent la nouvelle version de la note prenant les modifications souhaitées en compte et renvoyeent l'addresse de la nouvelle version de la note créer
     -La seule utilité de la methode virtuelle pure estAbstraite() est de rendre la classe Note abstraite. Son comportement (ne fait rien du tout) est défini dans les sous classe afin de rendre ces sous classes concrètes. Cette fonction pourra ne sera plus necessaire si une methode virtuelle pure utile vient à etre implementée plus tard
- 
- 
- 
+
+
+
  */
 
 class Article: public Note{
 private:
     std::string Texte;
     virtual void estAbstraite(){};
-    
+
 public:
     std::string getTexte() const;
     Article& setTexte(const std::string& t);
-    
+
     Article(const std::string& titre, const std::string& texte="");
 };
 
@@ -55,7 +55,7 @@ private:
     int Priorite;
     TIME::Date Echeance;
     virtual void estAbstraite(){};
-    
+
 public:
     std::string getAction() const {return Action;};
     int getPriorite() const {return Priorite;};
@@ -63,7 +63,7 @@ public:
     Tache& setAction(const std::string& s);
     Tache& setPriorite(int p);
     Tache& setEcheance(const TIME::Date& d);
-    
+
     Tache(const std::string& titre, const std::string& action, int priority=0);
 };
 
@@ -74,7 +74,7 @@ private:
     std::string Description;
     TypeMultimedia Type;
     virtual void estAbstraite(){};
-    
+
 public:
     std::string getAdresseFichierImage() const {return AdresseFichierImage;}
     std::string getDescription() const {return Description;};
@@ -82,8 +82,8 @@ public:
     Multimedia& setAdresseFichierImage(const std::string& f);
     Multimedia& setDescription(const std::string& d);
     Multimedia& setTypeMultimedia(const TypeMultimedia t);
-    
-    Multimedia(const std::string& titre, const std::string adressefichierimage, const std::string Description="", TypeMultimedia T=TypeMultimedia::image);
+
+    Multimedia(const std::string& titre, const std::string adressefichierimage, const std::string Description="", TypeMultimedia T=image);
 };
 
 
--- Code Source Projet LO21/main.cpp
+++ Code Source Projet LO21/main.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
+#include "PluriNotes.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here... modification test
-    
-    
+
+
     std::cout << "Hello, World!\n";
     return 0;
 }
