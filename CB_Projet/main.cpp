6d3c9ccecd9b4f73cbdfcdc3b988fdd783032172
anschlegel <antoine0996@gmail.com>
2017-05-21 15:14:55 +0200

    Tests main

--- CB_Projet/main.cpp
+++ CB_Projet/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include "timing.h"
+#include<iostream>
+#include<iomanip>
+#include <ctime>
+#include "PluriNotes.h"
 
 using namespace std;
+using namespace TIME;
 
 int main(int argc, const char * argv[])
 {
     // insert code here... modification test
 
-    cout << "Hello world!\n";
+   Date d;
+   d.afficher();
+   Horaire h;
+   h.afficher ();
+
+   Note n("soucis");
+   cout<<n.getdateModif()<<" "<<n.getdateCrea()<<"\n"<<n.gethoraireCrea()<<" "<<n.gethoraireModif()<<" "<<n.getactuel()<<" "<<n.gettitre();
+
+
     return 0;
 }
