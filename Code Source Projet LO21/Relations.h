29d0f66ef45d291465b3f49248de8775ff49443b
Anthony Galtier <anthony.galtier@gmail.com>
2017-05-27 12:51:53 +0200

    Déclarations des classes et méthodes essentielles correspondantes dans le fichier Relations.h

--- Code Source Projet LO21/Relations.h
+++ Code Source Projet LO21/Relations.h
@@ -3,17 +3,53 @@
 
 #include <stdio.h>
 #include "timing.h"
+#include "PluriNotes.h"
 #include <string>
 #include <typeinfo>
 
 using namespace TIME;
 
 
+class Couple {
+private:
+    std::string label;
+    Note* Note1;
+    Note* Note2;
+    
+public:
+    std::string getLabel() const {return label;}
+    Note* getNote1() const {return Note1;}
+    Note* getNote2() const {return Note2;}
+    void setLabel(const std::string& s){label=s;}
+    void setNotes1(Note* N){Note1=N;}
+    void setNotes2(Note* N){Note2=N;}
+    Couple(Note* A, Note* B, std::string l):Note1(A),Note2(B),label(l){};
+};
 
 
+class Relation{
+private:
+    std::string titre;
+    std::string description;
+    bool orientation;
+    Couple* couples;
+    int nbCouples;
+    int nbMaxCouples;
+    
+public:
+    bool getOrientation() const {return orientation;}
+    void setOrientation(bool b){orientation=b;}
+    void addCouple(Couple& c);
+    Relation(const std::string& t, const std::string& d, bool o);
+    ~Relation();
+};
 
+class Reference: public Relation {
 
-
+public:
+    Reference(const std::string& t, const std::string& d):Relation(t,d, true){};
+    ~Reference();
+};
 
 
 
