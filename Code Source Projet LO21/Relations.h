152172500822bbb727087551b2889251d3ddb7b2
Anthony Galtier <anthony.galtier@gmail.com>
2017-05-27 11:58:51 +0200

    Pour mieux s’y retrouver, j’ajoute deux fichier Relations.cpp et Relations.h au projet sur lequel seront traité justement traités tout ce qui concerne les couples, relations etc..

---
+++ Code Source Projet LO21/Relations.h
@@ -0,0 +1,22 @@
+#ifndef Relations_hpp
+#define Relations_hpp
+
+#include <stdio.h>
+#include "timing.h"
+#include <string>
+#include <typeinfo>
+
+using namespace TIME;
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* Relations_h */
